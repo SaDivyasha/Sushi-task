a8f3ab6291f34d1d60515534ccf38e8138fdfd66
SaDivyasha <159869259+SaDivyasha@users.noreply.github.com>
2024-07-21 15:07:22 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+import check from '../assets/check.svg'
+import star from '../assets/star.svg'
+import sushi12 from '../assets/sushi-12.png'
+import suhi11 from '../assets/sushi-11.png'
+import sushi10 from '../asset/sushi-10.png'
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+// init AOS animation
+AOS.init({
+    duration: 1000,
+    offset: 100,
+});
+
+// the additional code you saw in the video will not be needed :)
